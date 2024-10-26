655c0fde720febfc5c4592b8ae34f555890fcf9c
James <51017972+jpshanahan@users.noreply.github.com>
2024-10-26 18:18:48 +0800

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,12 +5,12 @@ const Index = () => {
     <div className="min-h-screen flex items-center justify-center bg-white-200">
       <div className="text-center">
         <img 
-          src="/head-striped-icon.svg" 
+          src="/head-striped-icon.png" 
           alt="Striped Head Icon" 
-          className="w-24 h-24 mx-auto mb-4 text-blue-800"
+          className="w-24 h-24 mx-auto mb-4 text-black-800"
         />
-        <h1 className="text-4xl font-bold mb-4 text-blue-800">Qonsol Prototype 5</h1>
-        <p className="text-xl text-blue-600">Prompt Library Manager</p>
+        <h1 className="text-4xl font-bold mb-4 text-black-800">Qonsol Prototype 5</h1>
+        <p className="text-xl text-black-600">Prompt Library Manager</p>
       </div>
     </div>
   );
