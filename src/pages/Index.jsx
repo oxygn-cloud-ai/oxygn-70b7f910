aab59863f49f333a27e0026b105e7adbf9adcf1d
James <51017972+jpshanahan@users.noreply.github.com>
2024-11-05 13:49:53 +0800

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,8 +5,9 @@ const Index = () => {
     <div className="min-h-screen flex items-center justify-center bg-white-200">
       <div className="text-center">
         <img 
-          src="https://static.wixstatic.com/media/611464_a195204321ec416081ffdd1660c85bde~mv2.png/v1/fill/w_196,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Cygnify-white-04.png"
+          //src="https://static.wixstatic.com/media/611464_a195204321ec416081ffdd1660c85bde~mv2.png/v1/fill/w_196,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Cygnify-white-04.png"
           // src="/head-striped-icon.png" 
+          src="cygnify-white.gif"
           alt="Striped Head Icon" 
           className="w-24 h-24 mx-auto mb-4 text-black-800"
         />
