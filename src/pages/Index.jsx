9d0ed2cb8ed18088d2bc909de311dcbc31e8b845
James <51017972+jpshanahan@users.noreply.github.com>
2024-11-05 14:13:25 +0800

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,6 +6,13 @@ const Index = () => {
       <div className="text-center">
         <div className="mb-4">
           <img 
+            src="head-striped-icon-wht.png"
+            alt="Qonsol" 
+            className="w-[10%] mx-auto"
+          />
+        </div>
+        <div className="mb-4">
+          <img 
             src="cygnify-white.gif"
             alt="Cygnify" 
             className="w-[20%] mx-auto"
@@ -13,13 +20,6 @@ const Index = () => {
         </div>
         <h1 className="text-4xl font-bold mb-4 text-white">Qonsol Prototype 6</h1>
         <p className="text-xl text-gray-300 mb-4">Cygnify Prompt Library Manager</p>
-        <div className="mb-4">
-          <img 
-            src="head-striped-icon-wht.png"
-            alt="Qonsol" 
-            className="w-[20%] mx-auto"
-          />
-        </div>
       </div>
     </div>
   );
