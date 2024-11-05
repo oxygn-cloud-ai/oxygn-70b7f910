5f32898b116b732152f2c642c53f16ecd6b7493a
James <51017972+jpshanahan@users.noreply.github.com>
2024-11-05 14:01:57 +0800

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
         <h1 className="text-4xl font-bold mb-4 text-white">Qonsol Prototype 6 for Cygnify</h1>
         <p className="text-xl text-gray-300 mb-4">Cygnify Prompt Library Manager</p>
         <img 
-          src="head-striped-icon.png"
+          src="head-striped-icon-wht.png"
           alt="Striped Head Icon" 
           className="mx-auto"
         />
